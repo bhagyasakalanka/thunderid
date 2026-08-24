0cc03a8cc19fdbebbdbc5cee9dda3013516a8c2d
bhagyasakalanka <bsakalanka9@gmail.com>
2026-08-24 09:48:10 +0530

    Capture an organization's configuration and apply it to a gateway
    Signed-off-by: bhagyasakalanka <bsakalanka9@gmail.com>

---
+++ backend/internal/gateway/service/exclusions.go
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package service
+
+import (
+	"github.com/thunder-id/thunderid/internal/gateway/bundle"
+)
+
+// withoutExcluded drops the held back resources from a bundle, so an apply pushes the same set the
+// promotion agreed on.
+func withoutExcluded(resources []bundle.Resource, excluded []string) []bundle.Resource {
+	if len(excluded) == 0 {
+		return resources
+	}
+	held := toSet(excluded)
+	out := make([]bundle.Resource, 0, len(resources))
+	for _, r := range resources {
+		if !held[r.Key()] {
+			out = append(out, r)
+		}
+	}
+	return out
+}
+
+func toSet(keys []string) map[string]bool {
+	set := make(map[string]bool, len(keys))
+	for _, k := range keys {
+		set[k] = true
+	}
+	return set
+}
